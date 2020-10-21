8d58df4211a26c53780d5a87ba3b7d18ef917625
sdonohue0918 <sdonohue0918@gmail.com>
2020-10-21 08:08:53 -0400

    added controller actions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,46 @@
 class SongsController < ApplicationController
+    def index
+        @songs = Song.all
+        render :json songs
+    
+    end
+
+    def show
+        @song = Song.find_by(id: params[:id])
+        render :json song
+
+    end
+
+    def new
+        @song = Song.new
+
+    
+    end
+
+    def create
+        @song = Song.create(song_params)
+
+    
+    end
+
+    def edit
+        @song = Song.find_by(id: params[:id])
+    end
+
+    def update
+        @song = Song.update(song_params)
+    end
+
+    def delete
+        @song = Song.find_by(id: params[:id])
+        @song.destroy!
+
+    end
+        
+    private
+
+    def song_params
+        params.require(:song).permit(:name, :author, :file)
+    end
+    
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,4 @@
 class Song < ApplicationRecord
+    has_one_attached :file
+    
 end
